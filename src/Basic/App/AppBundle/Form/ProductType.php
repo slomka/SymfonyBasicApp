2c71edacfe0a51d401f6d9ec4cc1798bbc6c2aab
slomka <seslo@op.pl>
2014-11-05 12:07:41 +0100

    Generate CRUD for an entity

---
+++ src/Basic/App/AppBundle/Form/ProductType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Basic\App\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Basic\App\AppBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'basic_app_appbundle_product';
+    }
+}
